00ae612f440e2f717dabc7f797ce9a0edd31defc
Jérôme Laban <jerome.laban@nventive.com>
2018-08-02 23:22:27 -0400

    Add Uno (iOS/Android/Wasm) targets.

---
+++ C#/Uno/RayTraceBenchmark/RayTraceBenchmark.Shared/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RayTraceBenchmark
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			RayTraceBenchmark.Console.WriteLineCallback = print;
+			BenchmarkMain.SaveImageCallback = showImage;
+			BenchmarkMain.Start();
+		}
+
+		private void print(string value)
+		{
+			secText.Text = value;
+		}
+
+		private void showImage(byte[] data)
+		{
+			var bitmap = new WriteableBitmap(Benchmark.Width, Benchmark.Height);
+			var pixelBuffer = bitmap.PixelBuffer;
+			var stream = pixelBuffer.AsStream();
+			data = BenchmarkMain.ConvertRGBToBGRA(data);
+			stream.Write(data, 0, data.Length);
+			stream.Flush();
+			image.Source = bitmap;
+		}
+	}
+}
